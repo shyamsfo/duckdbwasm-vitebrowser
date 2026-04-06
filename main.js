52fa663a0c524854d0f63d5b3506ce5ff75c1016
Shyam M <shyam.maniyedath@gmail.com>
2026-04-06 03:40:51 -0400

    sync

--- main.js
+++ main.js
@@ -1,53 +1,36 @@
-import './style.css'
-
 import * as duckdb from '@duckdb/duckdb-wasm';
-/*
 import duckdb_wasm from '@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url';
-import duckdb_wasm_next from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url';
 import mvp_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url';
+import duckdb_wasm_eh from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url';
 import eh_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url';
-
-import workers from './worker_blob.js'
- */
-
-import mvp_w from './mvp_worker.js'
-import eh_w from './eh_worker.js'
 
 const MANUAL_BUNDLES = {
     mvp: {
-        mainModule: 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-mvp.wasm',
-        mainWorker: mvp_w.mvp_worker
+        mainModule: duckdb_wasm,
+        mainWorker: mvp_worker,
     },
     eh: {
-        mainModule: 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.0/dist/duckdb-eh.wasm',
-        mainWorker: eh_w.eh_worker
+        mainModule: duckdb_wasm_eh,
+        mainWorker: eh_worker,
     },
 };
-
-console.log('Starting duck connection...')
-
 // Select a bundle based on browser checks
 const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
-
-var blob = new Blob(["(" + bundle.mainWorker.toString() + ")()"]);
-const worker = new Worker(URL.createObjectURL(blob, {type: 'text/javascript'}));
-// const worker = new Worker(URL.createObjectURL(blob));
-
-
 // Instantiate the asynchronus version of DuckDB-wasm
-// const worker = new Worker(bundle.mainWorker);
+const worker = new Worker(bundle.mainWorker);
 const logger = new duckdb.ConsoleLogger();
-
 const db = new duckdb.AsyncDuckDB(logger, worker);
 await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+console.log('Starting duck connection...')
+var blob = new Blob(["(" + bundle.mainWorker.toString() + ")()"]);
 
 const conn = await db.connect(); // Connect to db
-
 console.log('Completed duck connection...')
+
 let q = await conn.query('PRAGMA version');
 console.log("Version: ", q);
 async function do_version_check() {
-    q = await window.duck.conn.query(` select * from pragma_version();`); // Returns v = 101
+    q = await window.nu_duck.conn.query(` select * from pragma_version();`); // Returns v = 101
     console.log("Query result for pragma_version here: ...")
     console.log(q);
     console.log('**************************************************')
@@ -85,22 +68,22 @@ async function do_basic_queries() {
     );
 }
 
-window.duck = {};
+window.nu_duck = {};
 
-window.duck.conn = conn;
-window.duck.db = db;
-window.duck.worker = worker;
+window.nu_duck.conn = conn;
+window.nu_duck.db = db;
+window.nu_duck.worker = worker;
 
-console.log('duckdb wasm loading complete. variables available as window.duck.conn, window.duck.db and window.duck.worker')
+console.log('duckdb wasm loading complete. variables available as window.nu_duck.conn, window.nu_duck.db and window.nu_duck.worker')
 
 async function do_close() {
     // Closing everything
-    await window.duck.conn.close();
-    await window.duck.db.terminate();
-    await window.duck.worker.terminate();
-    window.duck = null;
+    await window.nu_duck.conn.close();
+    await window.nu_duck.db.terminate();
+    await window.nu_duck.worker.terminate();
+    window.nu_duck = null;
 }
 
 do_version_check();
-// do_basic_queries();
+//do_basic_queries();
 // do_close();
